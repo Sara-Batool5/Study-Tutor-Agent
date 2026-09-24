60b27f5df93e513bd08200026927b34656331b39
Sara-Batool5 <batoolsara764@gmail.com>
2026-09-24 13:03:30 +0500

    Create crew.py

---
+++ crew.py
@@ -0,0 +1,34 @@
+from crewai import Crew, Process
+
+from agents import create_study_tutor
+from tasks import create_tutor_task
+from memory import get_memory
+
+
+def run_tutor(question, level, mode):
+    """
+    Run the Study Tutor Agent.
+    """
+
+    tutor = create_study_tutor()
+
+    task = create_tutor_task(
+        agent=tutor,
+        question=question,
+        level=level,
+        mode=mode,
+    )
+
+    memory = get_memory()
+
+    crew = Crew(
+        agents=[tutor],
+        tasks=[task],
+        process=Process.sequential,
+        memory=memory,
+        verbose=True,
+    )
+
+    result = crew.kickoff()
+
+    return str(result)
